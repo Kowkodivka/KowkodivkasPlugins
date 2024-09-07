837bab4affce2c57ed229b42cddb0eef98ae4b6a
Kowkodivka <theeugenet@gmail.com>
2024-09-08 00:44:11 +0700

    uhh

--- plugins/HideAppLauncherButton/src/index.ts
+++ plugins/HideAppLauncherButton/src/index.ts
@@ -9,7 +9,7 @@ let unpatch: () => boolean;
 
 export default {
     onLoad() {
-        const blockList = ["lottie_icon"].map(n => getAssetIDByName(n));
+        const blockList = ["appsicon"].map(n => getAssetIDByName(n));
         unpatch = after("render", ChatInput.prototype, (_, ret) => {
             const input = findInReactTree(ret, t => "forceAnimateButtons" in t.props && t.props.actions);
             input.props.actions = input.props.actions.filter(a => !blockList.includes(a.source));
