74923dd60382741634d0e142f840665bd61f1f38
Kowkodivka <theeugenet@gmail.com>
2024-09-08 01:47:23 +0700

    skull skull

--- plugins/HideVoiceMessageButton/src/index.ts
+++ plugins/HideVoiceMessageButton/src/index.ts
@@ -9,7 +9,7 @@ let unpatch: () => boolean;
 
 export default {
     onLoad() {
-        const blockList = ["Microphone"].map(n => getAssetIDByName(n));
+        const blockList = ["ic_mic", "ic_mic_24px", "ic_mic_dark_circle", "ic_mic_light_circle", "ic_mic_neutral"].map(n => getAssetIDByName(n));
         unpatch = after("render", ChatInput.prototype, (_, ret) => {
             const input = findInReactTree(ret, t => "forceAnimateButtons" in t.props && t.props.actions);
             input.props.actions = input.props.actions.filter(a => !blockList.includes(a.source));
